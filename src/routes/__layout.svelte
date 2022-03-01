1c518bc1601202d22d9735303105aeb2fc70c864
Melborpco <accounts@melborp.co>
2022-03-01 09:32:18 -0500

    firest

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,15 @@
+<script>
+</script>
+
+<div class="home">
+	<slot />
+</div>
+
+<style global lang="postcss">
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+	.home {
+		@apply w-screen min-h-screen flex flex-col items-center justify-center;
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,10 @@
+<h1>Hello <span>world!</span></h1>
+
+<style lang="postcss">
+	h1 {
+		@apply text-3xl font-bold underline;
+		span {
+			@apply text-red-400;
+		}
+	}
+</style>
